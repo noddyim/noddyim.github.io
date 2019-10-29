1f0c3bb62550f98f85db2ce11dbd6e717d394dea
noddyim <56271668+noddyim@users.noreply.github.com>
2019-10-29 11:04:42 -0500

    Add files via upload

--- js/WallBreaker.js
+++ js/WallBreaker.js
@@ -137,20 +137,19 @@ function wall(colorW, xBrick, yBrick, wBrick, hBrick, index){
 		}*/
 		
 		if(x <= (xBrick+40) && (x+5) >=xBrick && (y+5) >= yBrick && y <= (yBrick+20)){
-			//alert("bx5:"+(x+5)+" bx:"+x+" by5:"+(y+5)+" by:"+y+" wx20:"+(xBrick+20)+" wx:"+xBrick+" wy40:"+(yBrick+40)+" wy:"+yBrick+" index:"+index);
 			c.fillStyle = colorW;
 			c.fillRect(-50, -50, 40, 20);
 			impact[index] = true;
 			xWall[index] = -50;
 			yWall[index] = -50;
 			if(x == (xBrick+40) || (x+5) == xBrick){
-				//alert("1brick x,y:"+xBrick+","+yBrick+" <br>ball x,y:"+x+","+y);
 				speedX = speedX * -1;
 			}
 			if((y+5) == yBrick || y == (yBrick+20)){
-				//alert("2brick x,y:"+xBrick+","+yBrick+" <br>ball x,y:"+x+","+y);
 				speedY = speedY * -1;
 			}
+			xBrick = xWall[index];
+			yBrick = yWall[index];
 		}else if(!impact[index]){
 			c.fillStyle = colorW;
 			c.fillRect(xBrick, yBrick, wBrick, hBrick);
@@ -192,37 +191,7 @@ function updateGameArea() {
 	for(var i = 0; i < 55; i++){
 		brick[i].crash();
 	}
-	/*updateRow1();
-	updateRow2();
-	updateRow3();
-	updateRow4();
-	updateRow5();*/
-  //updateBall();
 }
-
-/*function crashCheck(){
-
-	var crash = true;
-
-	var myleft = x;
-    var myright = x + 5;
-    var mytop = y;
-	var mybottom = y + 5;
-
-	var otherleft = otherobj.x;
-    var otherright = otherobj.x + 40;
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + 20;
-	
-	
-	if ((mybottom < othertop) ||
-    (mytop > otherbottom) ||
-    (myright < otherleft) ||
-    (myleft > otherright)) {
-      crash = false;
-    }
-    return crash;
-}*/
 
 function makeObstacles(){
 	var tempX = 50;
@@ -262,184 +231,4 @@ function makeObstacles(){
 			tempX += 45;
 		}
 	}
-	//alert("xwall:"+xWall);
-	//alert("ywall:"+yWall);
-}
-
-function updateRow1(){
-	c.fillStyle = "gold";
-	/*for(var i = 0; i < 12; i++)
-	{
-		c.fillRect(xWall[i], yWall[i], 40, 20);
-	}*/
-	if(50 <= x && x <= 90 && 50 <= y && y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(50, 50, 40, 20);
-	}
-	
-	if(95 <= x <= 135 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(95, 50, 40, 20);
-	}
-	if(140 <= x <= 180 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(140, 50, 40, 20);
-	}
-	if(185 <= x <= 225 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(185, 50, 40, 20);
-	}
-	if(230 <= x <= 270 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(230, 50, 40, 20);
-	}
-	if(275 <= x <= 315 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(275, 50, 40, 20);
-	}	 
-	if(320 <= x <= 360 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(320, 50, 40, 20);
-	}
-	if(365 <= x <= 405 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(365, 50, 40, 20);
-	}
-	if(410 <= x <= 450 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(410, 50, 40, 20);
-	}
-	if(455 <= x <= 500 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(455, 50, 40, 20);
-	}
-	if(500 <= x <= 540 && 50 <= y <=70){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(500, 50, 40, 20);
-	}
-}
-function updateRow2(){	
-	c.fillStyle = "red";
-	if(50 <= x <= 90 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(50, 75, 40, 20);
-	}	
-	if(95 <= x <= 135 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(95, 75, 40, 20);
-	}
-	if(140 <= x <= 180 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(140, 75, 40, 20);
-	}
-	if(185 <= x <= 225 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(185, 75, 40, 20);
-	}
-	if(230 <= x <= 270 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(230, 75, 40, 20);
-	}
-	if(275 <= x <= 315 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(275, 75, 40, 20);
-	}	 
-	if(320 <= x <= 360 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(320, 75, 40, 20);
-	}
-	if(365 <= x <= 405 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(365, 75, 40, 20);
-	}
-	if(410 <= x <= 450 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(410, 75, 40, 20);
-	}
-	if(455 <= x <= 500 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(455, 75, 40, 20);
-	}
-	if(500 <= x <= 540 && 75 <= y <=95){
-		c.fillRect(-50, -50, 40, 20);
-	}else{
-		c.fillRect(500, 75, 40, 20);
-	}
-	c.fillRect(50, 75, 40, 20);
-	c.fillRect(95, 75, 40, 20);
-	c.fillRect(140, 75, 40, 20);
-	c.fillRect(185, 75, 40, 20);
-	c.fillRect(230, 75, 40, 20);
-	c.fillRect(275, 75, 40, 20);
-	c.fillRect(320, 75, 40, 20);
-	c.fillRect(365, 75, 40, 20);
-	c.fillRect(410, 75, 40, 20);
-	c.fillRect(455, 75, 40, 20);
-	c.fillRect(500, 75, 40, 20);
-}
-
-function updateRow3(){	
-	c.fillStyle = "green";
-	c.fillRect(50, 100, 40, 20);
-	c.fillRect(95, 100, 40, 20);
-	c.fillRect(140, 100, 40, 20);
-	c.fillRect(185, 100, 40, 20);
-	c.fillRect(230, 100, 40, 20);
-	c.fillRect(275, 100, 40, 20);
-	c.fillRect(320, 100, 40, 20);
-	c.fillRect(365, 100, 40, 20);
-	c.fillRect(410, 100, 40, 20);
-	c.fillRect(455, 100, 40, 20);
-	c.fillRect(500, 100, 40, 20);
-}
-
-function updateRow4(){	
-	c.fillStyle = "blue";
-	c.fillRect(50, 125, 40, 20);
-	c.fillRect(95, 125, 40, 20);
-	c.fillRect(140, 125, 40, 20);
-	c.fillRect(185, 125, 40, 20);
-	c.fillRect(230, 125, 40, 20);
-	c.fillRect(275, 125, 40, 20);
-	c.fillRect(320, 125, 40, 20);
-	c.fillRect(365, 125, 40, 20);
-	c.fillRect(410, 125, 40, 20);
-	c.fillRect(455, 125, 40, 20);
-	c.fillRect(500, 125, 40, 20);
-}
-
-function updateRow5(){	
-	c.fillStyle = "cyan";
-	c.fillRect(50, 150, 40, 20);
-	c.fillRect(95, 150, 40, 20);
-	c.fillRect(140, 150, 40, 20);
-	c.fillRect(185, 150, 40, 20);
-	c.fillRect(230, 150, 40, 20);
-	c.fillRect(275, 150, 40, 20);
-	c.fillRect(320, 150, 40, 20);
-	c.fillRect(365, 150, 40, 20);
-	c.fillRect(410, 150, 40, 20);
-	c.fillRect(455, 150, 40, 20);
-	c.fillRect(500, 150, 40, 20);
 }
