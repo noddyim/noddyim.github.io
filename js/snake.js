7ad91ab3e996cd7c0f3f4ded1fafe27eedbf16eb
noddyim <56271668+noddyim@users.noreply.github.com>
2019-11-06 10:50:30 -0600

    Add files via upload

--- js/snake.js
+++ js/snake.js
@@ -3,14 +3,14 @@ const c = canvas.getContext('2d');
 
 var snakeHead;
 var apples;
-var appleXray = new Array(30);
-var appleYray = new Array(30);
-var bodyRay = new Array(30);
-var bodypart = new Array(30);
-var bodyXray = new Array(30);
-var bodyYray = new Array(30);
-var lastYray = new Array(1000);
-var lastXray = new Array(1000);
+var appleXray = new Array(51);
+var appleYray = new Array(51);
+var bodyRay = new Array(51);
+var bodypart = new Array(51);
+var bodyXray = new Array(51);
+var bodyYray = new Array(51);
+var lastYray = new Array(7000);
+var lastXray = new Array(7000);
 var temp;
 var count = 0;
 var bodycount = 0;
@@ -73,11 +73,12 @@ function output(){
 	}).length
 
 	document.getElementById("score").innerHTML = "Apples eaten: "+bodycount;
+	
 	//alert("lastx:"+lastXray+" bxray:"+bodyXray+" lasty:"+lastYray+" byray:"+bodyYray);
 }
 function body(){
 	this.updatebody = function(i){
-		if(x <= (appleXray[i]+10) && (x+10) >=appleXray[i] && (y+10) >= appleYray[i] && y <= (appleYray[i]+10)){
+		if(x <= (appleXray[i]+9) && (x+9) >=appleXray[i] && (y+9) >= appleYray[i] && y <= (appleYray[i]+9)){
 		//alert("collision");
 		appleXray[i] = -50;
 		appleYray[i] = -50;
@@ -85,68 +86,22 @@ function body(){
 		bodyRay[i] = true;
 		//get apples eaten
 		output();
-		//bodyXray[bodycount] = x+21;
-		//bodyYray[bodycount] = y+21;
-
-		//if(movedown){
-				//c.fillStyle = "black";
-				//c.fillRect(bodyXray[i], bodyYray[i], 20, 20);
-				//c.fillRect = (x, (y-21) , 20, 20);
-			//}
 		}else if(!bodyRay[i]){
-			c.fillStyle = "gold";
+			c.fillStyle = "limegreen";
 			c.fillRect(bodyXray[i], bodyYray[i], 10, 10);
 		}else if(bodyRay[i]){
-			c.fillStyle = "purple";
+			c.fillStyle = "limegreen";
 			c.fillRect(bodyXray[i], bodyYray[i], 10, 10);
+		}
+		if(i<(bodycount-1)){
+			if(x <= (bodyXray[i+1]+9) && (x+9) >=bodyXray[i+1] && (y+9) >= bodyYray[i+1] && y <= (bodyYray[i+1]+9)){
+				//alert("hit body");
+				stop();
+			}		
 		}
 	}
 }
-function crash(i){
-	if(x <= (appleXray[i]+2) && (x+2) >=appleXray[i] && (y+20) >= appleYray[i] && y <= (appleYray[i]+20)){
-		//alert("collision");
-		appleXray[i] = -50;
-		appleYray[i] = -50;
-		//give array true to increase bodycount
-		bodyRay[i] = true;
-		//get apples eaten
-		output();
-		//make snake longer
-		if(movedown){
-			//alert("movedown y+21 bcount:"+bodycount);
-			bodyXray[i] = x;
-			bodyYray[i] = y+21;
-			bodypart[i] = new body(i);
-			//bodypart[i].updatebody();
-			//bodypart[i] = new body(x, (y+21), bodycount);
-			//c.fillStyle = "black";
-			//c.fillRect = (x, (y+21) , 20, 20);
-		}
-		/*if(moveup){
-			alert("moveup y-21 bcount:"+bodycount);
-			//bodypart[i] = new body(x, (y-21), bodycount);
-			c.fillStyle = "black";
-			c.fillRect = (x, (y-21) , 20, 20);
-		}
-		if(moveleft){
-			alert("moveleft x-21 bcount:"+bodycount);
-			//bodypart[i] = new body((x-21), y, bodycount);
-			c.fillStyle = "black";
-			c.fillRect = ((x-21), y , 20, 20);
-		}
-		if(moveright){
-			alert("moveright x+21 bcount:"+bodycount);
-			//bodypart[i] = new body((x+21), y, bodycount);
-			c.fillStyle = "black";
-			c.fillRect = ((x+21), y , 20, 20);
-		}*/
-		//bodypart[bodycount] = new body(bodycount);
-	}/*else if(!bodyRay[i]){
-		c.fillStyle = "yellow"; //greenyellow
-		c.fillRect = (0, 0, 20, 20);
-	}*/
-}
-
+/*
 function paintbody(){
 	if(bodycount==1){
 		bodyXray[0]= x+21;
@@ -155,7 +110,7 @@ function paintbody(){
 		bodyXray[1]= x+42;
 		bodyYray[1]= y+42;
 	}
-}
+}*/
 
 function createbody(){
 
@@ -165,7 +120,7 @@ function createbody(){
 	}
 
 	for(var i = 0; i < 30; i++){
-		bodyRay[i]=false;
+		bodyRay[i] = false;
 		bodypart[i] = new body();
 	}
 }
@@ -173,14 +128,14 @@ function createbody(){
 function clear(){
 	c.clearRect(0, 0, canvas.width, canvas.height);
 	//paint apple
-	for(var i = 0; i < 30; i++){
+	for(var i = 0; i < 51; i++){
 		c.fillStyle = "red";
 		c.fillRect(appleXray[i], appleYray[i], w, h);
 	}
 	lastXray[count] = x;
 	lastYray[count] = y;
 	count++;
-	if(count==1000){
+	if(count==7000){
 		count = 0;
 	}
 }
@@ -191,10 +146,10 @@ function stop(){
 }
 function updateBackyard() {
 	clear();
+	//document.getElementById("xvalue").innerHTML = "X value: "+x;
+	//document.getElementById("yvalue").innerHTML = "Y value: "+y;
 	x = x + speedX;
 	y = y + speedY;
-	//bodyX = x;
-	//bodyY = y;
 	if(x > 590 || x < 1){
 		stop();
 	}
@@ -217,51 +172,106 @@ function updateBackyard() {
 function updateApple(){
 	appleX = Math.floor(Math.random() * 581);
 	appleY = Math.floor(Math.random() * 281);
+	
+	while(appleX % 10 !==0){
+		appleX = Math.floor(Math.random() * 581);
+	}
+	while(appleY % 10 !==0){
+		appleY = Math.floor(Math.random() * 281);
+	}
 
 	appleIndex++;
-
+	//alert("applex:"+appleX+" appley:"+appleY);
 	appleXray[appleIndex] = appleX;
 	appleYray[appleIndex] = appleY;
 }
 function moveSnake(){
 	//down
 	if(event.keyCode == 115 && !moveup){
-		speedX = 0;
-		speedY = 1;
-		movedown = true;
-		moveleft = false;
-		moveright = false;
-		moveup = false;
-		moving = true;
+		if(moveright){
+			while(x % 10 !== 0){
+				x++;
+			}
+		}
+		if(moveleft){
+			while(x % 10 !== 0){
+				x--;
+			}
+		}
+		if(x % 10 == 0){
+			speedX = 0;
+			speedY = 1;
+			movedown = true;
+			moveleft = false;
+			moveright = false;
+			moveup = false;
+			moving = true;
+		}
 	}
 	//left
 	if(event.keyCode == 97 && !moveright){
-		speedX = -1;
-		speedY = 0;
-		moveleft = true;
-		movedown = false;
-		moveright = false;
-		moveup = false;
-		moving = true;
+		if(movedown){
+			while(y % 10 !== 0){
+				y++;
+			}
+		}
+		if(moveup){
+			while(y % 10 !== 0){
+				y--;
+			}
+		}
+		if(x % 10 == 0){
+			speedX = -1;
+			speedY = 0;
+			moveleft = true;
+			movedown = false;
+			moveright = false;
+			moveup = false;
+			moving = true;
+		}
 	}
 	//right
 	if(event.keyCode == 100 && !moveleft){
-		speedX = 1;
-		speedY = 0;
-		moveright = true;
-		moveleft = false;
-		movedown = false;
-		moveup = false;
-		moving = true;
+		if(movedown){
+			while(y % 10 !== 0){
+				y++;
+			}
+		}
+		if(moveup){
+			while(y % 10 !== 0){
+				y--;
+			}
+		}
+		if(x % 10 == 0){
+			speedX = 1;
+			speedY = 0;
+			moveright = true;
+			moveleft = false;
+			movedown = false;
+			moveup = false;
+			moving = true;
+		}
 	}
 	//up
 	if(event.keyCode == 119 && !movedown){
-		speedX = 0;
-		speedY = -1;
-		moveup = true;
-		movedown = false;
-		moveleft = false;
-		moveright = false;
-		moving = true;
+		if(moveright){
+			while(x % 10 !== 0){
+				x++;
+			}
+		}
+		if(moveleft){
+			while(x % 10 !== 0){
+				x--;
+			}
+		}
+		if(x % 10 == 0){
+			speedX = 0;
+			speedY = -1;
+			moveup = true;
+			movedown = false;
+			moveleft = false;
+			moveright = false;
+			moving = true;
+		}
 	}
 }
