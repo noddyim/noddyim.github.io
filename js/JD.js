31396df85a4eeb7b4ace3e66bbc5c1087db6f9e6
noddyim <56271668+noddyim@users.noreply.github.com>
2022-05-03 18:12:52 -0500

    Create JD.js

---
+++ js/JD.js
@@ -0,0 +1,9 @@
+var myCity = new google.maps.Circle({
+                                    center:amsterdam,
+                                    radius:20000,
+                                    strokeColor:"#0000FF",
+                                    strokeOpacity:0.8,
+                                    strokeWeight:2,
+                                    fillColor:"#0000FF",
+                                    fillOpacity:0.4
+                                   });
