c1fe81d0205cccabeb75aac7fd211b4c12d436f2
noddyim <56271668+noddyim@users.noreply.github.com>
2022-05-03 18:21:09 -0500

    Delete JD.js

--- js/JD.js
+++
@@ -1,9 +0,0 @@
-var myCity = new google.maps.Circle({
-                                    center:Austin,
-                                    radius:20000,
-                                    strokeColor:"#0000FF",
-                                    strokeOpacity:0.8,
-                                    strokeWeight:2,
-                                    fillColor:"#0000FF",
-                                    fillOpacity:0.4
-                                   });
